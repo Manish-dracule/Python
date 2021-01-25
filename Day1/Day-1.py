86169ca7d2981da29f87e53a01ee009dec872ae4
Manish-dracule <guptamanish62229@gmail.com>
2021-01-25 22:33:58 +0530

    DAY 1 completed

---
+++ Day1/Day-1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Name to Number Convertor")
+firstName = input("Enter your First Name\n")
+lastName = input("Enter your Last Name\n")
+print("Your Name Size is",end=" ")
+print (len(firstName)+len(lastName))
